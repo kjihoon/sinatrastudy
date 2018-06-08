ab23ea4d49dcdb3ca7d284bf05bcf72cd8cf66f2
Kwak jihoon <kjihoon0914@naver.com>
2018-06-08 00:50:20 +0000

    sinatra-intro

--- app.rb
+++ app.rb
@@ -1,64 +1,64 @@
-# $ ruby app.rb -o $IP 필요!!
-
 require 'sinatra'
-require 'sinatra/reloader'
-# start web server
-# '/' root 주소 의미
+require "sinatra/reloader"
 
 get '/' do
-  'Hello world!'
+  'Hello world! welcome'
 end
 
 get '/htmlfile' do
-     send_file 'views/htmlfile.html'
+    send_file 'views/htmlfile.html'
 end
 
 get '/htmltag' do
-    '<h1>hihi</h1>
+    '<h1>html태그를 보낼수 있습니다.</h1>
     <ul>
-        <li>sdf</li>
-        <li>asdf</li>
-    </ul>
-    
+        <li>1</li>
+        <li>22</li>
+        <li>333</li>
+    </ul
     '
 end
 
-get '/name/:name' do
-   
-    "#{params[:name]}님 안녕하세요."
+get '/welcome/:name' do
+    "#{params[:name]}님 안녕하세요"
 end
 
 get '/cube/:num' do
-    num =params[:num]
-    num = num.to_i
-    num = num**3
-    "num #{num} 입니다."
+    # input = params[:num].to_i
+    # result = input ** 3
+    # "<h1>#{result}</h1>"
+    
+    "#{params[:num].to_i ** 3}"
+    
 end
 
 get '/erbfile' do
-    @name = "jihoon123123"
-    
-    erb:erbfile
+    @name = "5chang2"
+    erb :erbfile
 end
-
 
 get '/lunch-array' do
-    # 메뉴를 배열에 입력 및 저장
-    # 하나 추천
-    # erb일에 담아서 렌더링
-    luncharr = ["짜장면","카레"]
-    lunch = luncharr.sample
-    @lunch = lunch
+    # 메뉴들을 배열에 저장한다.
+    menu = ["20층", "김밥까페","시골집", "시래기"]
+    # 하나를 추첨한다.
+    @result = menu.sample
+    # erb 파일에 담아서 랜더링한다.
     erb :luncharray
 end
 
-
 get '/lunch-hash' do
-    menukey = ['0','1']
-    menu = {"0" =>"짜장면","1" =>"카레"}
-    imgpath = ['https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Jajangmyeon_by_stu_spivack.jpg/240px-Jajangmyeon_by_stu_spivack.jpg','http://recipe.ezmember.co.kr/cache/recipe/2016/06/29/82aedbd16108d4fedad9a4668333e8781.jpg']
-    rand =menukey.sample
-    @lunch = menu[rand]
-    @imgpath = imgpath[rand.to_i]
-    erb :luncharray
+    #메뉴들이 저장된 배열을 만든다
+    menu = ["짜장면", "볶음밥", "김밥"]
+    #메뉴 이름(key) 사진 url(value)을 
+    #가진 Hash 를 만든다
+    menu_img = {
+        "짜장면" => "http://www.ohfun.net/contents/article/images/2015/0607/1433677499050108.jpg",
+        "볶음밥" => "https://t1.daumcdn.net/cfile/tistory/136532424F08529921",
+        "김밥" => "https://i.ytimg.com/vi/60ANBnHjiDU/maxresdefault.jpg"
+    }
+    #랜덤으로 하나를 출력한다.
+    @menu_result = menu.sample
+    @menu_img = menu_img[@menu_result]
+    #이름과 url을 넘겨서 erb를 랜더링 한다.
+    erb :lunchhash
 end
